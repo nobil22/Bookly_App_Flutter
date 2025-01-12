4cb8d7292eacc0adf88ba2964ecd8781976610a2
nobil22 <mohamednabilmn833592@gmail.com>
2025-01-12 19:24:45 +0200

    create cusom app bar

---
+++ lib/Features/home/presentaion/views/wiedget/home_view_body.dart
@@ -0,0 +1,18 @@
+import 'package:bookly_app/Features/home/presentaion/views/wiedget/custom_app_bar.dart';
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        CustomAppBar(),
+      ],
+    );
+  }
+}
+
