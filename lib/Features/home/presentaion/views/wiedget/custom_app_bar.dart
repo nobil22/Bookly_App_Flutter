f3c086fa273753f6b3c8562ae4bbf48b3c6c146c
nobil22 <mohamednabilmn833592@gmail.com>
2025-01-18 15:49:11 +0200

    add style file and add text to home

--- lib/Features/home/presentaion/views/wiedget/custom_app_bar.dart
+++ lib/Features/home/presentaion/views/wiedget/custom_app_bar.dart
@@ -8,7 +8,7 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24,vertical: 40),
+      padding: const EdgeInsets.symmetric(vertical: 30),
       child: Row(
         children: [
           Image.asset(
--- lib/Features/home/presentaion/views/wiedget/home_view_body.dart
+++ lib/Features/home/presentaion/views/wiedget/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/Features/home/presentaion/views/wiedget/custom_app_bar.dart';
 import 'package:bookly_app/Features/home/presentaion/views/wiedget/feature_boobs_list_view.dart';
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'custom_list_view.dart';
@@ -10,13 +11,19 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-       
-        FeatureBoobsListView(),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeatureBoobsListView(),
+          SizedBox(
+            height: 35,
+          ),
+          Text('Best Seller',style: Styles.titleMediem),
+        ],
+      ),
     );
   }
 }
-
