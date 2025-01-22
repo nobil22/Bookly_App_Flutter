12b3826013147dfa17e28a2a0db45f39634f640a
nobil22 <mohamednabilmn833592@gmail.com>
2025-01-22 02:10:09 +0200

    best seller ui done

--- lib/Features/home/presentaion/views/wiedget/best_seller_list_view_item.dart
+++ lib/Features/home/presentaion/views/wiedget/best_seller_list_view_item.dart
@@ -1,3 +1,5 @@
+import 'package:bookly_app/Features/home/presentaion/views/wiedget/book_rating.dart';
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
@@ -26,17 +28,44 @@ class BestSellerListViewItem extends StatelessWidget {
           SizedBox(
             width: 30,
           ),
-          Column(
-            children: [
-              SizedBox(
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
                   width: MediaQuery.of(context).size.width * .5,
                   child: Text(
                     'Harry Potter and the Goblet of Fire ',
-                    style: Styles.textStyle20,
+                    style: Styles.textStyle20.copyWith(
+                      fontFamily: kGtSectraFine,
+                    ),
                     overflow: TextOverflow.ellipsis,
                     maxLines: 2,
-                  )),
-            ],
+                  ),
+                ),
+                SizedBox(
+                  height: 3,
+                ),
+                Text(
+                  'J.K Rollwing',
+                  style: Styles.textStyle14,
+                ),
+                SizedBox(
+                  height: 3,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      '19.99',
+                      style: Styles.textStyle20
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    const Spacer(),
+                    BookRating(),
+                  ],
+                )
+              ],
+            ),
           )
         ],
       ),
---
+++ lib/Features/home/presentaion/views/wiedget/book_rating.dart
@@ -0,0 +1,33 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class BookRating extends StatelessWidget {
+  const BookRating({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          FontAwesomeIcons.solidStar,
+          color: Colors.yellow,
+        ),
+        SizedBox(
+          width: 6.3,
+        ),
+        Text(
+          '4.8',
+          style: Styles.textStyle16,
+        ),
+         SizedBox(
+          width: 5,
+        ),
+         Text(
+          '(256)',
+          style: Styles.textStyle14.copyWith(color: Colors.grey),
+        ),
+      ],
+    );
+  }
+}
